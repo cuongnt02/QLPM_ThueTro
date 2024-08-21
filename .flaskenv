6cc0738274f7120f4769e79148ffdc54f3ee7f70
cuongnt02 <cuong.ntc02@gmail.com>
2024-08-21 17:04:54 +0700

    bug fixes

--- .flaskenv
+++ .flaskenv
@@ -1,1 +1,2 @@
 FLASK_APP=index.py
+DATABASE_URI=mysql+pymysql://root:0308110299Go@localhost/thuetro?charset=utf8mb4
--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_admin import Admin
+from flask_admin.theme import Bootstrap4Theme
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -12,7 +13,8 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 
-# Initialize Flask-Admin with Bootstrap 4 theme
-admin = Admin(app, name='THUETRO ADMIN', template_mode='bootstrap4')
+# Initialize Flask-Admin with Bootswatch theme (pull from github new version)
+admin = Admin(app, name='THUETRO ADMIN',
+              theme=Bootstrap4Theme(swatch='cyborg'))
 
 from app import routes, models, admin_config
