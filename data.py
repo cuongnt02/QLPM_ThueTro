77513c0605a7a9028a2c4f62cd3e1fcc7de0d0ad
cuongnt02 <cuong.ntc02@gmail.com>
2024-09-20 17:21:12 +0700

    final db setting

--- data.py
+++ data.py
@@ -78,7 +78,6 @@ def create_message(sender_id, receiver_id, content):
 if __name__ == "__main__":
 
     with app.app_context():
-        db.create_all()
 
         # Tạo users
         u1 = create_user("cuong", "cuong@gmail.com", "NTC", "123456", "USER")
@@ -94,46 +93,46 @@ if __name__ == "__main__":
             print("Lỗi: Không thể thêm người dùng do trùng lặp email.")
 
         # Tạo motels
-        m1 = create_motel(address="Go Vap, Ho Chi Minh", max_room=3, user_id=u2.id)
-        m2 = create_motel(address="Phu Nhuan, Ho Chi Minh", max_room=4, user_id=u2.id)
-        db.session.add_all([m1, m2])
-        db.session.commit()
+        # m1 = create_motel(address="Go Vap, Ho Chi Minh", max_room=3, user_id=u2.id)
+        # m2 = create_motel(address="Phu Nhuan, Ho Chi Minh", max_room=4, user_id=u2.id)
+        # db.session.add_all([m1, m2])
+        # db.session.commit()
 
         # Tạo rooms
-        r1 = create_room("Phòng 101", 2000000, "Phòng trọ thoáng mát, gần siêu thị, sân bay.", 100000, 100000, m1.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417992/Working/tro1_uqndaa.jpg")
-        r2 = create_room("Phòng 102", 2500000, "Phòng trọ thoáng mát, gần siêu thị, sân bay.", 150000, 150000, m1.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417990/Working/tro2_imkkzq.jpg")
-        r3 = create_room("Phòng 201", 3000000, "Phòng rộng, tiện nghi.", 120000, 120000, m2.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417989/Working/tro3_fnnjli.jpg")
-        db.session.add_all([r1, r2, r3])
-        db.session.commit()
+        # r1 = create_room("Phòng 101", 2000000, "Phòng trọ thoáng mát, gần siêu thị, sân bay.", 100000, 100000, m1.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417992/Working/tro1_uqndaa.jpg")
+        # r2 = create_room("Phòng 102", 2500000, "Phòng trọ thoáng mát, gần siêu thị, sân bay.", 150000, 150000, m1.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417990/Working/tro2_imkkzq.jpg")
+        # r3 = create_room("Phòng 201", 3000000, "Phòng rộng, tiện nghi.", 120000, 120000, m2.id, picture="https://res.cloudinary.com/driiz3taz/image/upload/v1724417989/Working/tro3_fnnjli.jpg")
+        # db.session.add_all([r1, r2, r3])
+        # db.session.commit()
 
         # Sử dụng múi giờ 'Asia/Ho_Chi_Minh'
-        timezone = pytz.timezone('Asia/Ho_Chi_Minh')
+        # timezone = pytz.timezone('Asia/Ho_Chi_Minh')
 
         # Tạo posts
-        p1 = create_post("Phòng trọ tại Gò Vấp", r1.description, u1.id, r1.id, timezone)
-        p2 = create_post("Phòng trọ tại Phú Nhuận", r2.description, u1.id, r2.id, timezone)
-        p3 = create_post("Phòng trọ rộng tại Phú Nhuận", r3.description, u2.id, r3.id, timezone)
-        db.session.add_all([p1, p2, p3])
-        db.session.commit()
+        # p1 = create_post("Phòng trọ tại Gò Vấp", r1.description, u1.id, r1.id, timezone)
+        # p2 = create_post("Phòng trọ tại Phú Nhuận", r2.description, u1.id, r2.id, timezone)
+        # p3 = create_post("Phòng trọ rộng tại Phú Nhuận", r3.description, u2.id, r3.id, timezone)
+        # db.session.add_all([p1, p2, p3])
+        # db.session.commit()
 
         # Tạo bookings
-        b1 = create_booking(u1.id, r1.id, datetime.now(), datetime.now() + timedelta(days=30), 6000000)
-        b2 = create_booking(u2.id, r2.id, datetime.now(), datetime.now() + timedelta(days=15), 3750000)
-        db.session.add_all([b1, b2])
-        db.session.commit()
+        # b1 = create_booking(u1.id, r1.id, datetime.now(), datetime.now() + timedelta(days=30), 6000000)
+        # b2 = create_booking(u2.id, r2.id, datetime.now(), datetime.now() + timedelta(days=15), 3750000)
+        # db.session.add_all([b1, b2])
+        # db.session.commit()
 
         # Tạo reviews
-        rev1 = create_review(u1.id, r1.id, 4, "Phòng tốt nhưng hơi ồn.")
-        rev2 = create_review(u2.id, r2.id, 5, "Phòng rất tốt, sạch sẽ.")
-        db.session.add_all([rev1, rev2])
-        db.session.commit()
+        # rev1 = create_review(u1.id, r1.id, 4, "Phòng tốt nhưng hơi ồn.")
+        # rev2 = create_review(u2.id, r2.id, 5, "Phòng rất tốt, sạch sẽ.")
+        # db.session.add_all([rev1, rev2])
+        # db.session.commit()
 
         # Tạo payments
 
         # Tạo messages
-        msg1 = create_message(u1.id, u2.id, "Xin chào, phòng còn trống không?")
-        msg2 = create_message(u2.id, u1.id, "Xin chào, phòng còn trống. Bạn muốn thuê khi nào?")
-        db.session.add_all([msg1, msg2])
-        db.session.commit()
+        # msg1 = create_message(u1.id, u2.id, "Xin chào, phòng còn trống không?")
+        # msg2 = create_message(u2.id, u1.id, "Xin chào, phòng còn trống. Bạn muốn thuê khi nào?")
+        # db.session.add_all([msg1, msg2])
+        # db.session.commit()
 
         print("Dữ liệu đã được thêm thành công!")
